8dde9a3070c65a023192fa1b7f2ef55452b34376
JPeregrinT <128410529+JPeregrinT@users.noreply.github.com>
2023-07-04 20:05:51 +0200

    Añadimos historial reciente al dashboard

--- src/Components/Dashboard/History/History.js
+++ src/Components/Dashboard/History/History.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import history from '../History/History.css'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { dateFormat } from '../../utils/dateFormat';
+import { Button } from 'react-bootstrap';
 
-const History = () => {
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+const HistoryList = ({ refresh }) => {
+  const [historyList, setHistoryList] = useState([]);
+
+  const historysGetter = async () => {
+    const userId = window.localStorage.getItem('userId');
+    console.log(userId);
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/v1/get-history/${userId}`);
+      setHistoryList(data);
+      console.log('esto es data', data);
+    } catch (error) {
+      console.log('Error get History', error);
+    }
+  };
+
+  useEffect(() => {
+    historysGetter();
+  }, [refresh]);
+
   return (
-    <div className='history__container'>
+    <div className='history_card'>
       <h3>Recent History</h3>
+      {historyList.map(({ _id, title, amount, date, category, description }) => (
+        <div key={_id} className='items__historys'>
+          <div className='icon'></div>
+          <h5>{title}</h5>
+          <p>{amount}</p>
+          <date>{dateFormat(date)}</date>
+          <p>{category}</p>
+          <p>{description}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default History
+export default HistoryList;
--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -2,44 +2,43 @@ import React from 'react'
 import Menu from '../Menu'
 import '../Dashboard/Dashboard.css'
 import LinesChart from './Chart/Chart'
-import History from './History/History'
+import HistoryList from './History/History';
 
 const Dashboard = () => {
-
-
   return (
     <>
-      <Menu/>
+      <Menu />
       <div className='dashboard__container'>
         <h2 className='title__all__transactions'>All transactions</h2>
         <div className='total__salary'>
           <p className='balance__title'>Balance</p>
-          <input type='text' value={'178,69$'} className='balance' disabled></input>
+          <input type='text' value={'178,69$'} className='balance' disabled />
         </div>
         <div className='history__container'>
-            <History/>
+          <HistoryList />
+        </div>
+        <div className='chart__container'>
+          <LinesChart />
+        </div>
+        <div className='amount__container'>
+          <div className='total__incomes'>
+            <h2>Total Incomes</h2>
+            {/* <p>{dollar} {totalIncome()}</p> */}
           </div>
-          <div className='chart__container'><LinesChart/></div>
-          <div className='amount__container'>
-            <div className='total__incomes'>
-              <h2>Total Incomes</h2>
-              {/* <p>{dollar} {totalIncome()}</p> */}
-            </div>
 
-            <div className='total__expenses'>
-              <h2>Total Expenses</h2>
-              {/* <p>{dollar} {totalExpense()}</p> */}
-            </div>
-
-            <div className='total__balance'>
-              <h2>Total Balance</h2>
-              {/* <p>{dollar} {totalBalance()}</p> */}
-            </div>
+          <div className='total__expenses'>
+            <h2>Total Expenses</h2>
+            {/* <p>{dollar} {totalExpense()}</p> */}
           </div>
-          
+
+          <div className='total__balance'>
+            <h2>Total Balance</h2>
+            {/* <p>{dollar} {totalBalance()}</p> */}
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Dashboard
